6df022030197eae0e914de5bf54615537c8acb57
VPbanna123 <vijaypalsinghrathore8959179600@gmail.com>
2025-08-30 09:48:22 +0530

    Update main.py

--- py/main.py
+++ py/main.py
@@ -356,8 +356,8 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "main:app", 
-        host="127.0.0.1", 
-        port=5002, 
+         host="0.0.0.0",  
+        port=10000,   
         reload=False, 
         log_level="info"
     )
